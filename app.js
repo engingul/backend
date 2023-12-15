be0790a7c32a8052102bc94383667073e2622c75
engingul <egul1987@gmail.com>
2023-12-15 18:41:01 +0300

    4. odev tamamlandı

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+require('./app_api/models/db');
 var app = express();
 
 app.use(logger('dev'));
